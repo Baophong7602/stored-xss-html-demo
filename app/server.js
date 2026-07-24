352ba2ee151e2c34870d6580ac2a41e976078221
Baophong7602 <baophong7602@gmail.com>
2026-07-24 20:20:58 +0700

    fix code

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const sanitizeHtml = require("sanitize-html");
 
 const app = express();
 
@@ -8,6 +9,7 @@ const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -16,6 +18,36 @@ app.use(
 
 app.use(express.static("public"));
 
+function sanitizeInput(value) {
+  value = value
+    .replace(/<script/gi, "&lt;script")
+    .replace(/<\/script>/gi, "&lt;/script&gt;");
+
+  return sanitizeHtml(value, {
+    allowedTags: [
+      "b",
+      "i",
+      "u",
+      "strong",
+      "em",
+      "br",
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+      "p",
+      "span",
+      "div",
+      "ul",
+      "ol",
+      "li",
+    ],
+    allowedAttributes: {},
+  });
+}
+
 app.get("/", (req, res) => {
   const data = JSON.parse(fs.readFileSync("db.json"));
 
@@ -23,28 +55,22 @@ app.get("/", (req, res) => {
     comments: data.comments || [],
   });
 });
+
 app.post("/comment", (req, res) => {
   const data = JSON.parse(fs.readFileSync("db.json"));
 
   data.comments.push({
-    name: req.body.name,
-
+    name: sanitizeInput(req.body.name),
     email: req.body.email,
-
     website: req.body.website,
-
-    comment: req.body.comment,
+    comment: sanitizeInput(req.body.comment),
   });
 
-  fs.writeFileSync(
-    "db.json",
-
-    JSON.stringify(data, null, 4),
-  );
+  fs.writeFileSync("db.json", JSON.stringify(data, null, 4));
 
   res.redirect("/");
 });
 
 app.listen(PORT, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log(`Server running at http://localhost:${PORT}`);
 });
